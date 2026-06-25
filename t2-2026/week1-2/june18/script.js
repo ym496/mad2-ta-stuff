4d04650a6a1c99b5cb210cb8cadf802a7d474e63
Vinay Maurya <24f2005644@ds.study.iitm.ac.in>
2026-06-26 01:31:01 +0530

    Create script.js with JavaScript basics examples
    Added JavaScript basics including DOM manipulation, scope, hoisting, and function examples.

---
+++ t2-2026/week1-2/june18/script.js
@@ -0,0 +1,74 @@
+//session link - https://drive.google.com/file/d/1K3cMERgYm9rwHIQgSC9PByEfosdoT2RG/view
+
+// javascript basics
+
+// topics to be covered
+
+// Document Object Model (DOM)
+// what is a block {}
+// Scope and Hoisting
+// call stack
+
+// const divtag = document.getElementById("content");
+
+// divtag.innerHTML = "<h1>Hello World from JS</h1>";
+
+// if(true){ console.log("Hello from if block");
+// console.log("Hello from outside if block"); }
+
+// {const a = 10;
+// let b = 10;
+// var c ="good";
+// console.log(a);
+// console.log(b);
+// }
+
+// console.log(c);
+
+// function myfunction(){
+
+//     let d = "hello";
+//     console.log(d);
+// }
+
+// myfunction();
+// console.log(d);
+
+
+// console.log(a);
+
+// function myfunction(){
+//     console.log(a);
+// }
+
+// myfunction()
+// console.log(b);
+// console.log(james);
+// console.log(a);
+
+let result = myfunction(3,4);
+console.log(result);
+
+const james = "bond"
+
+let a = 10;
+
+var b = 20;
+
+function myfunction(a,b){   
+    return a + b;
+   }
+console.log(func);
+
+
+// let result2 = func(3,4);
+// console.log(result2);
+ 
+var func = function(a,b){
+    return a + b;
+}
+
+console.log(func);
+
+
+
