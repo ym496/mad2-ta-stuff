d2065ae2a03ef96b6177f9c91fe3df1ada95776c
Vinay Maurya <24f2005644@ds.study.iitm.ac.in>
2026-07-04 23:05:19 +0530

    Comment out app1 and Vue app code in script.js
    Comment out Vue app and related functionality in script.js.

---
+++ t2-2026/week3/july4/script.js
@@ -0,0 +1,136 @@
+
+
+
+
+// new Vue({
+//   el: "#app",
+//   data: {
+//     message: "Hello Vue!"
+//   }
+// });
+
+
+// new Vue({
+//   el:"#app",
+//   data:{
+//     name:"Vinay",
+//     age:21
+//   }
+// })
+
+// Data Binding
+
+// new Vue({
+//   el:"#app",
+//   data:{
+//     imageUrl:"https://picsum.photos/200"
+//   }
+// })
+
+// Event Handling
+
+// new Vue({
+//   el:"#app",
+//   methods:{
+//     sayHello(){
+//       alert("Hello Students")
+//     }
+//   }
+// })
+
+// new Vue({
+//   el:"#app",
+//   data:{
+//     count:0
+//   }
+// })
+
+
+// new Vue({
+//   el:"#app",
+//   data:{
+//     name:""
+//   }
+// })
+
+// new Vue({
+//   el:"#app",
+//   data:{
+//     show:true
+//   }
+// })
+
+
+// new Vue({
+//   el:"#app",
+//   data:{
+//     fruits:[
+//       "Apple",
+//       "Mango",
+//       "Banana",
+//       "Orange"
+//     ]
+//   }
+// })
+
+
+
+// new Vue({
+//   el:"#app",
+//   data:{
+//     name:"Vinay",
+//     course:"Data Science"
+//   }
+// })
+
+
+
+
+// new Vue({
+//   el:"#app",
+
+//   data:{
+//     newTask:"",
+//     tasks:[]
+//   },
+
+//   methods:{
+//     addTask(){
+
+//       if(this.newTask.trim()){
+
+//         this.tasks.push(this.newTask)
+
+//         this.newTask=""
+
+//       }
+
+//     }
+//   }
+// })
+
+// const app1 = {
+//   count: 10
+// };
+
+// function render() {
+//   document.getElementById("app1").innerText = app1.count;
+// }
+
+// render();
+
+// setInterval(() => {
+//   app1.count--;
+//   render();
+// }, 1000);
+
+// const app = new Vue({
+//   el: "#app",
+//   data: {
+//     count: 10
+//   }
+// });
+
+// setInterval(() => {
+//   app.count--;
+// }, 1000); 
